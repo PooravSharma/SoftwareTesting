02f4ab640635d5d42fff9643bdc121431f7080ff
AlladuhCars <142006995+AlladuhCars@users.noreply.github.com>
2024-05-12 16:38:31 +0800

    Add files via upload

--- Planner-24/Planner-24/tests/BlackBoxOrganization.java
+++ Planner-24/Planner-24/tests/BlackBoxOrganization.java
@@ -1,22 +1,79 @@
 package tests;
 
 import au.edu.sccs.csp3105.NBookingPlanner.Organization;
+import au.edu.sccs.csp3105.NBookingPlanner.Person;
+import au.edu.sccs.csp3105.NBookingPlanner.Room;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.ArrayList;
+import org.junit.jupiter.api.DisplayName;
 import org.mockito.Mockito;
 
 public class BlackBoxOrganization {
 
+	//Create a spy of the Organization class to 
+	Organization planner = Mockito.spy(Organization.class);
+
+
+	private ArrayList<Person> employees;
+	private ArrayList<Room> rooms;
+	
+	 void Organization(){
+		employees = new ArrayList<Person>();
+		employees.add(new Person("Justin Gardener"));
+		employees.add(new Person("Ashley Matthews"));
+		employees.add(new Person("Mary Jane Cook"));
+		employees.add(new Person("Rose Austin"));
+		employees.add(new Person("Mike Smith"));
+		employees.add(new Person("Helen West"));
+		employees.add(new Person("Steven Lewis"));
+		employees.add(new Person("Edith Cowan"));
+		employees.add(new Person("Mark Colin"));
+		employees.add(new Person("Jacquie Martin"));
+		employees.add(new Person("Jaci Johnston"));
+		employees.add(new Person("Travis Colin"));
+		employees.add(new Person("Ashley Martin"));
+		
+
+
+		rooms = new ArrayList<Room>();
+		rooms.add(new Room("JO18.330"));
+		rooms.add(new Room("JO7.221"));
+		rooms.add(new Room("JO15.236"));
+		rooms.add(new Room("JO1.230"));
+		rooms.add(new Room("JO34.536"));
+		rooms.add(new Room("JO19.230"));
+		rooms.add(new Room("ML5.123"));
+		rooms.add(new Room("ML18.330"));
+		rooms.add(new Room("ML21.520"));
+		rooms.add(new Room("ML13.213"));
+		rooms.add(new Room("ML21.310"));
+		rooms.add(new Room("ML13.218"));
+	}
+	
+	public ArrayList<Person> getEmployees() {
+		return employees;
+	}
+
+	public ArrayList<Room> getRooms() {
+		return rooms;
+	}
+	
+	
+	//We define variables which will be called depending on their respective variable names
 	String isRoom = "This ID matches an existing room"; 
 	String isNotRoom = "This ID does not match an existing room"; 
 	
 	String isEmployee = "This person is an employee"; 
 	String isNotEmployee = "This person is NOT an employee"; 
 	
-
 	
+	
+	
+	// we can capture
 	@Test
 	void testRoomTrue(String ID) {
 		assertEquals(Organization.getRoom(ID), isRoom);
@@ -26,12 +83,13 @@ public class BlackBoxOrganization {
 		assertEquals(Organization.getRoom(ID), isNotRoom);
 	}
 
+	//continuing this, each employee
 	void testEmployeeTrue(String name) {
-		assertEquals(Organization.getEmployee();
+		assertEquals(Organization.getEmployee(employees), isEmployee);
 	}
 	
 	void testEmployeeNotTrue(String name) {
-		assertEquals(Organization.getEmployee(isNotEmployee));
+		assertEquals(Organization.getEmployee());
 	}
 	
 	
