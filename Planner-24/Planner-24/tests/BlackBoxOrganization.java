2d3190f714709d24dc5ecd90c951920e741e6791
AlladuhCars <142006995+AlladuhCars@users.noreply.github.com>
2024-05-16 18:48:07 +0800

    Add files via upload

--- Planner-24/Planner-24/tests/BlackBoxOrganization.java
+++ Planner-24/Planner-24/tests/BlackBoxOrganization.java
@@ -17,40 +17,53 @@ public class BlackBoxOrganization {
 	//Create a spy of the Organization class to initialise and store a mockup of data from the file
 	Organization planner = Mockito.spy(Organization.class);
 
+	private ArrayList<Person> employees;
+	private ArrayList<Room> rooms;
+	
+	public void Organization(){
 	//Create an array of strings with the names of employees
-	String[] expectedPersonOutput = new String[] {
-			"Justin Gardener",
-	        "Ashley Matthews",
-	        "Mary Jane Cook",
-	        "Rose Austin",
-	        "Mike Smith",
-	        "Helen West",
-	        "Steven Lewis",
-	        "Edith Cowan",
-	        "Mark Colin",
-	        "Jacquie Martin",
-	        "Jaci Johnston",
-	        "Travis Colin",
-	        "Ashley Martin",
+	employees = new ArrayList<Person>();
+	employees.add(new Person("Justin Gardener"));
+	employees.add(new Person("Ashley Matthews"));
+	employees.add(new Person("Mary Jane Cook"));
+	employees.add(new Person("Rose Austin"));
+	employees.add(new Person("Mike Smith"));
+	employees.add(new Person("Helen West"));
+	employees.add(new Person("Steven Lewis"));
+	employees.add(new Person("Edith Cowan"));
+	employees.add(new Person("Mark Colin"));
+	employees.add(new Person("Jacquie Martin"));
+	employees.add(new Person("Jaci Johnston"));
+	employees.add(new Person("Travis Colin"));
+	employees.add(new Person("Ashley Martin"));
+			
+	
+	
+	rooms = new ArrayList<Room>();
+	rooms.add(new Room("JO18.330"));
+	rooms.add(new Room("JO7.221"));
+	rooms.add(new Room("JO15.236"));
+	rooms.add(new Room("JO1.230"));
+	rooms.add(new Room("JO34.536"));
+	rooms.add(new Room("JO19.230"));
+	rooms.add(new Room("ML5.123"));
+	rooms.add(new Room("ML18.330"));
+	rooms.add(new Room("ML21.520"));
+	rooms.add(new Room("ML13.213"));
+	rooms.add(new Room("ML21.310"));
+	rooms.add(new Room("ML13.218"));
 			
 	};
 	
-	String[] expectedRoomOutput = new String[] {
-			"JO18.330",
-			"JO7.221",
-			"JO15.236",
-			"JO1.230",
-			"JO34.536",
-			"JO19.230",
-			"ML5.123",
-			"ML18.330",
-			"ML21.520",
-			"ML13.213",
-			"ML21.310",
-			"ML13.218",
-			
-	};
+	public ArrayList<Person> getEmployees() {
+		return employees;
+	}
+
+	public ArrayList<Room> getRooms() {
+		return rooms;
+	}
 	
+
 	//We define variables which will be called depending on their respective variable names
 	String isRoom = "This ID matches an existing room"; 
 	String isNotRoom = "This ID does not match an existing room"; 
@@ -58,26 +71,30 @@ public class BlackBoxOrganization {
 	String isEmployee = "This person is an employee"; 
 	String isNotEmployee = "This person is NOT an employee"; 
 	
+	String output; 
 	
 	
-	
-	// we can capture
+	/*
+	 *  each test returns a room if the room itself exists. 
+	 *  Exceptions are there for if the room does not exist
+	 */
 	@Test
 	void testRoomTrue(String ID) throws Exception {
-		assertEquals(expectedRoomOutput, isRoom);
+		for (room : rooms)
+		assertEquals(rooms, isRoom);
 	}
 	
 	void testRoomNotTrue(String ID) throws Exception {
-		assertEquals(expectedRoomOutput, isNotRoom);
+		 assertEquals(output, isNotRoom);
 	}
 
 	//continuing this, each employee
 	void testEmployeeTrue(String name) throws Exception {
-		assertEquals(expectedPersonOutput, isEmployee);
+		assertEquals(output, isEmployee);
 	}
 	
 	void testEmployeeNotTrue(String name) throws Exception {
-		assertEquals(expectedPersonOutput, isNotEmployee);
+		assertEquals(output, isNotEmployee);
 	}
 	
 	
