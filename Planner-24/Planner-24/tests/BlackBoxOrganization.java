c9aabb2635b88a39b1a745a4e39b586a0193fe05
AlladuhCars <142006995+AlladuhCars@users.noreply.github.com>
2024-05-17 19:29:39 +0800

    Add files via upload

--- Planner-24/Planner-24/tests/BlackBoxOrganization.java
+++ Planner-24/Planner-24/tests/BlackBoxOrganization.java
@@ -5,6 +5,7 @@ import au.edu.sccs.csp3105.NBookingPlanner.Person;
 import au.edu.sccs.csp3105.NBookingPlanner.Room;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
@@ -15,52 +16,21 @@ import org.mockito.Mockito;
 
 public class BlackBoxOrganization {
 
-
+	private Organization organization;
+	
 	private ArrayList<Person> employees;
 	private ArrayList<Room> rooms;
 	
 	public void Organization(){
-	//Create an array of strings with the names of employees. these strings will be the expected input asnd output
+	//Create an array of strings with the names of employees. these strings will be the expected input and output
 	employees = new ArrayList<Person>();
-	employees.add(new Person("Justin Gardener"));
-	employees.add(new Person("Ashley Matthews"));
-	employees.add(new Person("Mary Jane Cook"));
-	employees.add(new Person("Rose Austin"));
-	employees.add(new Person("Mike Smith"));
-	employees.add(new Person("Helen West"));
-	employees.add(new Person("Steven Lewis"));
-	employees.add(new Person("Edith Cowan"));
-	employees.add(new Person("Mark Colin"));
-	employees.add(new Person("Jacquie Martin"));
-	employees.add(new Person("Jaci Johnston"));
-	employees.add(new Person("Travis Colin"));
-	employees.add(new Person("Ashley Martin"));
-			
-	
+	employees.add(new Person("Justin Gardener"));	
 	
 	rooms = new ArrayList<Room>();
 	rooms.add(new Room("JO18.330"));
-	rooms.add(new Room("JO7.221"));
-	rooms.add(new Room("JO15.236"));
-	rooms.add(new Room("JO1.230"));
-	rooms.add(new Room("JO34.536"));
-	rooms.add(new Room("JO19.230"));
-	rooms.add(new Room("ML5.123"));
-	rooms.add(new Room("ML18.330"));
-	rooms.add(new Room("ML21.520"));
-	rooms.add(new Room("ML13.213"));
-	rooms.add(new Room("ML21.310"));
-	rooms.add(new Room("ML13.218"));
 			
 	};
-	
-	public ArrayList<Person> getEmployees() {
-		return employees;
-	}
 
-	public ArrayList<Room> getRooms() {
-		return rooms;
-	}
 	
 
 	//We define variables which will be called depending on their respective variable names
@@ -80,56 +50,61 @@ public class BlackBoxOrganization {
 	@DisplayName("BB_org_0 valid room")
 	public void TestRoom(String ID) throws Exception {
 		//Create a spy of the Organization class using Mockito to monitor interactions
-		Organization organization = Mockito.spy(Organization.class);
+		organization = Mockito.spy(Organization.class);
 		
-		Room[] actualoutput = new Room [] {"JO18.330"};
+		String actualoutput = "JO18.330";
 		
-		for (Room[] tocheck : rooms) {
-			if(tocheck.getID().equals(ID)) {
-				actualoutput = tocheck;
-				assertEquals(rooms, actualoutput);
+		for (Room tocheck : rooms) {
+			assertEquals(tocheck.getID(), actualoutput);
+			System.out.print(isRoom);
 			}
-		}
-		
+				
 	}
 	
 	@Test
 	@DisplayName("BB_org_1 invalid room")
 	public void testRoomNotTrue(String ID) throws Exception {
 		
-		Organization organization = Mockito.spy(Organization.class);
+		organization = Mockito.spy(Organization.class);
 
-		Room actualoutput = null;
+		String actualoutput = "JO18.33";
+		
 		for (Room tocheck : rooms) {
-			if(tocheck.getID().equals(ID)) {
-				actualoutput = tocheck;
-				assertEquals(rooms, actualoutput);
+			assertEquals(tocheck.getID(), actualoutput);
+			System.out.print(isNotRoom);
+
 			}
-		}
+		
 	}
 
-	//continuing this, each employee
+	//continuing this, each employee is tested for a string
 	
 	@Test
 	@DisplayName("BB_org_2 valid employee")
 	public void testEmployeeTrue(String name) throws Exception {
 		
-		Organization organization = Mockito.spy(Organization.class);
+		organization = Mockito.spy(Organization.class);
 
-		Person actualoutput = null;
+		String actualoutput = "Justin Gardener";
+		for (Person tocheck : employees) {
+			assertEquals(tocheck.getName(), actualoutput);
+			System.out.print(isEmployee);
+		}
 		
-		assertEquals(employees, actualoutput);
 	}
 	
 	@Test
 	@DisplayName("BB_org_3 invalid employee")
 	public void testEmployeeNotTrue(String name) throws Exception {
 		
-		Organization organization = Mockito.spy(Organization.class);
+		organization = Mockito.spy(Organization.class);
 
-		Person actualoutput = null;
+		String actualoutput = "Just Garden";
 		
-		assertEquals(employees, actualoutput);
+		for (Person tocheck : employees) {
+			assertEquals(tocheck.getName(), actualoutput);
+			System.out.print(isNotEmployee);
+		}
 	}
 	
 	
